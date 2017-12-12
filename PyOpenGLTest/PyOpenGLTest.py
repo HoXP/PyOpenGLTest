7f0dea0ac151bdf4b3267db3e357e0cb423d03b6
HoXP <hxptow@163.com>
2017-12-12 18:05:22 +0800

    s
    添加注释

--- PyOpenGLTest/PyOpenGLTest.py
+++ PyOpenGLTest/PyOpenGLTest.py
@@ -3,11 +3,11 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 import os
 import numpy
-
-class VAO_IDs:
+#用class实现Python所不支持的enum
+class VAO_IDs:  #定义VAO名称枚举
     Triangles = 0
     NumVAOs = 1
-class Buffer_IDs:
+class Buffer_IDs:   #定义VBO名称枚举
     ArrayBuffer = 0
     NumBuffers = 1
 class Attrib_IDs:
@@ -16,24 +16,24 @@ class Attrib_IDs:
 VAOs = [None] * VAO_IDs.NumVAOs
 Buffers = [None] * Buffer_IDs.NumBuffers
 
-NumVertices = 6
+NumVertices = 6 #顶点数
 
-def CreateShader(shader_type, source):
-    shader = glCreateShader(shader_type)
-    glShaderSource(shader, source)
-    glCompileShader(shader)
-    if glGetShaderiv(shader, GL_COMPILE_STATUS) != GL_TRUE:
-        raise RuntimeError(glGetShaderInfoLog(shader))
+def CreateShader(shaderType, source):
+    shader = glCreateShader(shaderType)    #创建着色器对象，并得到着色器对象名（一个非0整数）
+    glShaderSource(shader, source)  #将着色器源码关联到着色器对象上
+    glCompileShader(shader) #编译着色器源码
+    if glGetShaderiv(shader, GL_COMPILE_STATUS) != GL_TRUE: #如果编译状态不为GL_TRUE，即编译失败
+        raise RuntimeError(glGetShaderInfoLog(shader))  #则返回日志信息
     return shader
 
-def LoadFile(filename):
+def LoadFile(filename): #获取文件中的文本
     with open(os.path.join(os.path.dirname(__file__), filename)) as fp:
         return fp.read()
 
 def init():
-    VAOs[VAO_IDs.Triangles] = glGenVertexArrays(VAO_IDs.NumVAOs)
-    glBindVertexArray(VAOs[VAO_IDs.Triangles])
-
+    VAOs[VAO_IDs.Triangles] = glGenVertexArrays(VAO_IDs.NumVAOs)    #生成VAO，并得到VAO名（一个非0整数）
+    glBindVertexArray(VAOs[VAO_IDs.Triangles])  #绑定VAO
+    #顶点数据
     data = numpy.array([-0.90, -0.90,   #Triangle 1
                         0.85, -0.90,
                         -0.90,  0.85,
@@ -50,11 +50,11 @@ def init():
     fsStr = LoadFile('fs.shader')
     vertShader = CreateShader(GL_VERTEX_SHADER, vsStr)
     fragShader = CreateShader(GL_FRAGMENT_SHADER, fsStr)
-    shaderProgram = glCreateProgram()
-    glAttachShader(shaderProgram, vertShader)
+    shaderProgram = glCreateProgram()   #创建空的着色器程序
+    glAttachShader(shaderProgram, vertShader)   #将着色器对象关联到着色器程序上
     glAttachShader(shaderProgram, fragShader)
-    glLinkProgram(shaderProgram)
-    glUseProgram(shaderProgram)
+    glLinkProgram(shaderProgram)    #链接着色器程序
+    glUseProgram(shaderProgram) #使用着色器程序处理顶点片元
 
     glVertexAttribPointer(Attrib_IDs.vPosition, 2, GL_FLOAT, GL_FALSE, 0, None)
     glEnableVertexAttribArray(Attrib_IDs.vPosition)
